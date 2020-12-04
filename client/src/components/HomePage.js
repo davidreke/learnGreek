0fa1447f39bcac5c6bf902eb3365c70c7a9b7da5
davidreke <54542172+davidreke@users.noreply.github.com>
2020-12-04 12:13:39 -0600

    fixed mimnesko greek on homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -6,7 +6,7 @@ export default class HomePage extends Component {
             <div className="pt-20" id="frontpage">
                 <div id="about" className="front-panel">
                     <h2>About</h2>
-                    <p lang="el">Mimnéskó (μιμνήσκομαι) is an app to allow users to quiz themselves on the Koine Greek Language.</p>
+                    <p lang="el">Mimnéskó (μιμνήσκω) is an app to allow users to quiz themselves on the Koine Greek Language.</p>
                     <p>Mimensko is built using Bootstrap, React.JS, Express.JS, MongoDB, Node.JS, and JWT. The application features mobile-responsiveness and and repition based learning algorithm</p>
                 </div>
                 <div id="status" className="front-panel">
